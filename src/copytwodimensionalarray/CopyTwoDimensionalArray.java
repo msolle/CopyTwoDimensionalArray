f590f1cda7ea3d93917d15e170eceef2f259c189
msolle <msolle90@gmail.com>
2014-05-26 13:48:57 -0400

    Added problem header

--- src/copytwodimensionalarray/CopyTwoDimensionalArray.java
+++ src/copytwodimensionalarray/CopyTwoDimensionalArray.java
@@ -2,6 +2,9 @@ package copytwodimensionalarray;
 
 /**
  *
+ * Problem taken from Data Structures & Algorithms In Java 5th Edition
+ * R-3.1 "Give a Java code fragment to clone a two-dimensional array, a, of float entries to another two-dimensional array, b, of similar type and size."
+ * 
  * @author Matt
  */
 public class CopyTwoDimensionalArray {
